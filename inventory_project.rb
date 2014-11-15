d91fbaba6d37d64b01da9587ff3d9cae694e8ed4
Shane Barringer <shane.barringer@outlook.com>
2014-11-15 03:58:29 -0500

    cleaned up code and made the program more interactive.

--- inventory_project.rb
+++ inventory_project.rb
@@ -1,32 +1,99 @@
-@items = {"Founder's KBS" => 17,
-		"Bell's HopSlam" => 11,
-		"Founder's Porter" => 19,
-		"Anderson Valley Burboun Barrel Stout" => 9}
+@kbs = "Founder's KBS"
+@hop = "Bell's HopSlam"
+@port = "Founder's Porter"
+@av = "Anderson Valley Burboun Barrel Stout"
+
+@items = {@kbs => 17,
+		 @hop => 11,
+		@port => 19,
+		@av => 9}
 
 def list_items
 	@items.each do |beer, quantity|
 		puts "We have #{quantity} bottles of #{beer}"
 	end
+	
+	puts " "
+	puts "Would you like to add anything else? (Y/N)"
+	
+	@answer = gets.chomp.downcase
+		if @answer == "y" || @answer == "yes"
+			edit_items
+		else
+			clear
+			puts "Thanks! Make sure to enjoy a tasty beverage before you leave!"
+		end
 end
 
-list_items
+def prompt 
+	clear
 
-puts "Would you like to edit these items?"
-answ = gets.chomp.downcase
-
-if answ == "yes" || answ == "y" 
 	puts "Please enter the item you would like to edit:"
-	@input = gets.chomp.downcase
+	puts ""
+	puts "Enter A for Founder's KBS"
+	puts "Enter B for Bell's Hopslam"
+	puts "Enter C for Founder's Porter"
+	puts "Enter D for Anderson Valley Burboun Barrel Stout"
+	puts ""
+	
+	puts "Type LIST for an up-to-date inventory list"
+	puts " "
+	puts "Type NEW to add a new inventory item"
+	
+end
 
-		if @input == "founder's kbs"
-			@input = "Founder's KBS"
-			puts "Great! What is the new quantity"
-			@update = gets.chomp.to_i
-			@items[@input] = @update
-			puts "The total has been updated to #{@update} bottles of #{@input}."
-		end
+def edit_items 
+	clear
+	prompt
+	@input = gets.chomp.upcase
+	
+	until @input == "A"  || @input == "B"  ||  @input == "C"  ||  @input == "D"  || @input == "NEW" || @input == "LIST"
+		clear
+		puts "You have entered an invalid response"
+		puts "Press ENTER to continue"
+		puts "Press CTRL-C to EXIT"
+		gets.chomp
+		prompt
+		@input = gets.chomp.upcase 
+	end
+				
+	if @input == "A"
+		@input = @kbs
+	elsif @input == "B"
+		@input = @hop
+	elsif @input == "C"
+		@input = @port
+	elsif @input == "D"
+		@input = @av
+	elsif @input == "NEW"
+		clear
+		puts "Please enter a name for the NEW inventory item"
+		@input = gets.chomp
+	elsif @input == "LIST"	
+		clear
+		list_items		
+	end
+
+	clear
+	puts "Great! What is the new quantity of #{@input}"
+	@update = gets.chomp.to_i
+	@items[@input] = @update
+	
+	clear
+	puts "The total has been updated to #{@update} bottles of #{@input}"
 
 	list_items
 
 end
+
+def clear
+	system "clear" or system "cls"
+end
+
+
+list_items
+
+	# if @input == "Q"
+	# 	puts "Thanks for helping out with the inventory!"
+	# end
 
